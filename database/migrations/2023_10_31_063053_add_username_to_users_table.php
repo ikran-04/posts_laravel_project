a1fd3f6814a0381bf8d1cbdf8a0ab327e3ad8258
ikran-04 <hikraan581@gmail.com>
2023-10-31 09:35:48 +0300

    adding username column migration

--- database/migrations/2023_10_31_063053_add_username_to_users_table.php
+++ database/migrations/2023_10_31_063053_add_username_to_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-              $table->string('username');
+            $table->string('username');
+
         });
     }
 
@@ -22,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-              $table->dropColumn('username');
+             $table->dropColumn('username');
         });
     }
 };
